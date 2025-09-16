00a9a8cc0427d55de025b4eed055a93fd28b172e
ra399 <rajeshnenavath38@gmail.com>
2025-09-16 18:44:06 +0530

    Update firebase.js

--- gruhini-master/gruhini-main/src/data/firebase.js
+++ gruhini-master/gruhini-main/src/data/firebase.js
@@ -4,15 +4,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "gruhini-efac0.firebaseapp.com",
-  projectId: "gruhini-efac0",
-  storageBucket: "gruhini-efac0.appspot.com",
-  messagingSenderId: "692630689537",
-  appId: "1:692630689537:web:2fbb3a02d5c36c17abed00"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
